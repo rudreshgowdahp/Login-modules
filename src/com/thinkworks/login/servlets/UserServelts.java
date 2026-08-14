568edf0303d2f97442472f0db9eac3669e1c9b7d
rudresh <rudreshgowdahp@gmail.com>
2026-08-14 21:11:34 +0530

    add

--- src/com/thinkworks/login/servlets/UserServelts.java
+++ src/com/thinkworks/login/servlets/UserServelts.java
@@ -11,23 +11,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+
 @WebServlet(urlPatterns = "/CreateLogin")
 
 public class UserServelts extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         UserDTO userDTO = new UserDTO();
+        userDTO.setLoginId(req.getParameter("LoginId"));
         userDTO.setName(req.getParameter("name"));
         userDTO.setEmail(req.getParameter("email"));
         userDTO.setPhoneNumber(req.getParameter("phoneNumber"));
         userDTO.setPassword(req.getParameter("password"));
         userDTO.setConfirmPassword(req.getParameter("confirmPassword"));
 
-        System.out.println(userDTO.toString());
-
 
         UserService userService = new UserServiceImp();
-        String result = userService.validateAndSave(userDTO);
+        String result = null;
+        try {
+            result = userService.validateAndSave(userDTO);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
 
         if (result.equals("Registration Successful")) {
@@ -45,6 +53,7 @@ public class UserServelts extends HttpServlet {
             dispatcher.forward(req, resp);
 
         }else{
+             req.setAttribute("id",userDTO.getLoginId());
             req.setAttribute("name",userDTO.getName());
             req.setAttribute("email",userDTO.getEmail());
             req.setAttribute("phoneNumber",userDTO.getPhoneNumber());
