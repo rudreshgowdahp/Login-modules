3444364acf08bf929d15cdcb3f7a43368b55a494
rudresh <rudreshgowdahp@gmail.com>
2026-08-16 16:55:04 +0530

    add

--- src/com/thinkworks/login/service/UserService.java
+++ src/com/thinkworks/login/service/UserService.java
@@ -2,10 +2,12 @@ package com.thinkworks.login.service;
 
 import com.thinkworks.login.DTO.UserDTO;
 
+import java.sql.SQLException;
+
 public interface UserService {
 
     // Registration validation
-    String validateAndSave(UserDTO userDTO);
+    String validateAndSave(UserDTO userDTO) throws SQLException, ClassNotFoundException;
 
 
 }
