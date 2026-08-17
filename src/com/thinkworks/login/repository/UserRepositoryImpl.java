bad580b65d5545169b2874a8605cfd814257fd0f
rudresh <rudreshgowdahp@gmail.com>
2026-08-17 22:20:20 +0530

    add

--- src/com/thinkworks/login/repository/UserRepositoryImpl.java
+++ src/com/thinkworks/login/repository/UserRepositoryImpl.java
@@ -5,6 +5,50 @@ import com.thinkworks.login.DTO.UserDTO;
 import java.sql.*;
 
 public class UserRepositoryImpl implements UserRepository{
+    @Override
+    public UserDTO findByEmail(String email) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("findByEmail() called");
+        String name = "root";
+        String password = "Rudra@9019";
+        String url = "jdbc:mysql://localhost:3306/thinkworks";
+        Connection connection = DriverManager.getConnection(url,name,password);
+        String sql = "SELECT email, password FROM userdetails WHERE email = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,email);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            UserDTO user = new UserDTO();
+            user.setEmail(resultSet.getString("email"));
+            user.setPassword(resultSet.getString("password"));
+            return user;
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public UserDTO findById(int id) throws SQLException {
+        System.out.println("findById() called");
+        String name = "root";
+        String password = "Rudra@9019";
+        String url = "jdbc:mysql://localhost:3306/thinkworks";
+        Connection connection = DriverManager.getConnection(url,name,password);
+        String sql = "select * from userdetails where id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1,id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setName(resultSet.getString(2));
+            userDTO.setEmail(resultSet.getString(3));
+            userDTO.setPhoneNumber(resultSet.getString(4));
+            userDTO.setPassword(resultSet.getString(5));
+            return userDTO;
+        }
+        return null;
+    }
 
     @Override
     public UserDTO findAll() throws SQLException {
